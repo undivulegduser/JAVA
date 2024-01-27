ae56d13cf831ab8e94c1c629bf8fb136a8870845
undivulegduser <147341573+undivulegduser@users.noreply.github.com>
2024-01-27 11:17:58 +0530

    Add files via upload

---
+++ bool1.java
@@ -0,0 +1,9 @@
+public class bool1 {
+    public static void main(String args[]) {
+        boolean java = true;
+        boolean cpp= false;
+        System.out.println(java);
+        System.out.println(cpp);
+
+    } 
+}
